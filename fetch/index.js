69e869fef681ca6f8f85294895d40817aef0626b
czM1K3 <misamadera1@gmail.com>
2022-04-05 15:38:02 +0200

    Both methods

--- fetch/index.js
+++ fetch/index.js
@@ -2,20 +2,23 @@ import express from "express";
 import fetch from "node-fetch";
 import cors from "cors";
 
-const app = express();
-
-app.use(cors({
-	origin: "*",
-}))
-
-app.post("/api/buses", async (_req, res) => {
+const getBusses = async (_req, res) => {
 	const raw = await fetch("https://mhd.kacis.eu/api/buses", {
 		method: "POST",
 		body: '{"key":"869f749a-1b72-4ccb-b888-cb0aef8e0aea"}',
 	});
 	const data = await raw.json();
 	res.status(200).json(data);
-});
+};
+
+const app = express();
+
+app.use(cors({
+	origin: "*",
+}))
+
+app.post("/api/buses", getBusses);
+app.get("/api/buses", getBusses);
 
 app.use(express.static('public',{
 	cacheControl: true,
