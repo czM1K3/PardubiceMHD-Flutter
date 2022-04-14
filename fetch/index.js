175153fdd30f95cb9fb284c90a67c8ba015d8ab3
czM1K3 <misamadera1@gmail.com>
2022-04-14 21:42:52 +0200

    Added try catch

--- fetch/index.js
+++ fetch/index.js
@@ -4,29 +4,37 @@ import cors from "cors";
 import compression from "compression";
 
 const getBusses = async (_req, res) => {
-	const raw = await fetch("https://mhd.kacis.eu/api/buses", {
-		method: "POST",
-		body: '{"key":"09658f18-0f8c-4909-a571-0bc6b4acfbec"}',
-	});
-	const data = await raw.json();
-	res.status(200).json(data);
+	try {
+		const raw = await fetch("https://mhd.kacis.eu/api/buses", {
+			method: "POST",
+			body: '{"key":"09658f18-0f8c-4909-a571-0bc6b4acfbec"}',
+		});
+		const data = await raw.json();
+		res.status(200).json(data);
+	} catch {
+		res.status(500).json({ error: "Failed to fetch data" });
+	}
 };
 
 const app = express();
 
-app.use(cors({
-	origin: "*",
-}));
+app.use(
+	cors({
+		origin: "*",
+	})
+);
 
 app.use(compression());
 
 app.post("/api/buses", getBusses);
 app.get("/api/buses", getBusses);
 
-app.use(express.static('public',{
-	cacheControl: true,
-	maxAge: 60 * 60 * 24,
-}));
+app.use(
+	express.static("public", {
+		cacheControl: true,
+		maxAge: 60 * 60 * 24,
+	})
+);
 
 app.listen(3000, () => {
 	console.log("Server is running on port 3000");
