54133d80cfa271bc1abeee1d86e8d40460d71fbc
czM1K3 <misamadera1@gmail.com>
2022-04-25 11:47:22 +0200

    Fix

--- lib/widgets/downloadButton.dart
+++ lib/widgets/downloadButton.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:html' as html;
+import 'package:pardumhd/functions/getUrl.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DownloadAppButton extends StatelessWidget {
   const DownloadAppButton({Key? key}) : super(key: key);
@@ -7,8 +8,9 @@ class DownloadAppButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      onPressed: () {
-        html.window.open("/app.apk", "App");
+      onPressed: () async {
+        Uri _url = Uri.parse(getUrl() + "app.apk");
+        if (!await launchUrl(_url)) throw 'Could not launch $_url';
       },
       icon: const Icon(Icons.android),
     );
